af68331c493b385c194daa7009e4e5b821fe7ce8
Christopher Carrasco <christopher.carrasco@outlook.com>
2025-11-28 00:27:36 +0100

    gh-pages installed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-init/', // set repo name for github pages rendering
   plugins: [
     react()
   ]
